a8803e9db6ef9f9e415a1062731dea3950788508
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-03 19:33:21 -0700

    Create Buildfreedom2.java

---
+++ src/me/Creepercoders/Buildfreedom2/Buildfreedom2.java
@@ -0,0 +1,36 @@
+package me.Creepercoders.Buildfreedom2;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Buildfreedom2 extends JavaPlugin{
+  public final Logger logger = Logger.getLogger("Minecraft");
+	public static Buildfreedom2 plugin;
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + pdfFile.getVersion() + " Has Been Disabled!");
+	}	
+	
+    @Override
+    public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
+    }	
+    
+    public boolean onCommand(CommandSender sender, Command cmd,  String commandLabel, String[] args){
+        Player player = (Player) sender;
+    	if(commandLabel.equalsIgnoreCase("")){
+    
+        }
+		return false;   
+    }
+    
+}    
+    
