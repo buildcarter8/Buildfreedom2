8f66a74eaa4cd79d55a295d1d7e021cd06cbaf76
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-03 21:02:36 -0700

    Update Buildfreedom2.java

--- src/me/Creepercoders/Buildfreedom2/Buildfreedom2.java
+++ src/me/Creepercoders/Buildfreedom2/Buildfreedom2.java
@@ -1,37 +1,38 @@
 package me.Creepercoders.Buildfreedom2;
 
 import java.util.logging.Logger;
-
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Buildfreedom2 extends JavaPlugin{
-  public final Logger logger = Logger.getLogger("Minecraft");
-	public static Buildfreedom2 plugin;
-	public final String creators = (ChatColor.RED + "Version: 1.0 by Buildcarter8 & xXWilee999Xx");
-	@Override
-	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + pdfFile.getVersion() + " Has Been Disabled!");
-	}	
-	
-    @Override
-    public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
-    }	
-    
-    public boolean onCommand(CommandSender sender, Command cmd,  String commandLabel, String[] args){
-        Player player = (Player) sender;
-    	if(commandLabel.equalsIgnoreCase("buildfreedom")){
-    		sender.sendMessage(buildfreedom2.creators)
-    
-        }
-		return false;   
+public class Buildfreedom2 extends JavaPlugin
+{
+    private Logger log = Logger.getLogger("Minecraft");
+
+    public void onEnable()
+    {
+        log.info("[Buildfreedom2] - Enabled! - v1.1 by buildcarter8 and xXWilee999Xx");
+    }
+
+    public void onDisable()
+    {
+        log.info("[Buildfreedom2] - Disabled.");
     }
     
-}    
-    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+    Player player = null;
+    if (sender instanceof Player)
+        {
+            player = (Player)sender;
+    }
+        
+        if(cmd.getName().equalsIgnoreCase("buildfreedom2"))
+        {
+        	sender.sendMessage(ChatColor.AQUA + "Buildfreedom2 by buildcarter8 and xXWilee999Xx");
+        }
+		return false;
+    }
+}
