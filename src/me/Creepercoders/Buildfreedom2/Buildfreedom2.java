7f472e52a6ac080c91dd34377b1d23e79fbf7682
buildcarter8 <tylerhuff2002@me.com>
2013-05-04 00:15:25 -0300

    Update Buildfreedom2.java

--- src/me/Creepercoders/Buildfreedom2/Buildfreedom2.java
+++ src/me/Creepercoders/Buildfreedom2/Buildfreedom2.java
@@ -11,7 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Buildfreedom2 extends JavaPlugin{
   public final Logger logger = Logger.getLogger("Minecraft");
 	public static Buildfreedom2 plugin;
-	
+	public final String creators = (ChatColor.RED + "Version: 1.0 by Buildcarter8 & xXWilee999Xx");
 	@Override
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
@@ -26,7 +26,8 @@ public class Buildfreedom2 extends JavaPlugin{
     
     public boolean onCommand(CommandSender sender, Command cmd,  String commandLabel, String[] args){
         Player player = (Player) sender;
-    	if(commandLabel.equalsIgnoreCase("")){
+    	if(commandLabel.equalsIgnoreCase("buildfreedom")){
+    		sender.sendMessage(buildfreedom2.creators)
     
         }
 		return false;   
