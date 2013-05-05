8f4e494181a2082f2bc84020ed1975a238462358
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-05 00:49:40 -0700

    added buildfreedom2 (command)

---
+++ src/me/Creepercoders/Buildfreedom2/Commands/Command_buildfreedom2.java
@@ -0,0 +1,17 @@
+package me.Creepercoders.Buildfreedom2.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command_buildfreedom2 extends BF2_Command
+{
+  @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+		sender.sendMessage(ChatColor.AQUA + "Buildfreedom2 2.0 by buildcarter8 and xXWilee999Xx");
+		return false;
+        
+    }
+}
